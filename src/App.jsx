4fa60be306f2b12cb2827c9b0e7492072fdec6da
tainamiranda2 <tainaferreira0987@gmail.com>
2024-01-16 14:51:56 -0300

    iniciando o repositorio do projeto

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+//import { useState } from 'react'
+//import reactLogo from './assets/react.svg'
+//import viteLogo from '/vite.svg'
+//import './App.css'
+
+function App() {
+ // const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <h1>Salvados vidas</h1>
+     <button>Orientações</button>
+     <button>Idealizadores</button>
+
+     <button>Play</button>
+
+    </>
+  )
+}
+
+export default App
