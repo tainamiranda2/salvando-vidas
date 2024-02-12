3f99b5ed0e12000d7299223b8ee96b36e474f486
tainamiranda2 <tainaferreira0987@gmail.com>
2024-02-12 12:00:34 -0300

    iniciando a base para a fase 2

--- src/pages/jogo/JogoOne.jsx
+++ src/pages/jogo/JogoOne.jsx
@@ -1,16 +1,15 @@
+
+
 import { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+
 import homem from '../../img/homem.png';
 import vitoria from '../../img/vitoria.png';
 import cardio from '../../img/cardio.webp';
-import joelho from '../../img/joelho.png';
-import respiracao from '../../img/respiracao.png';
-
-
-
+//import joelho from '../../img/joelho.png';
+//import respiracao from '../../img/respiracao.png';
 
 export const JogoOne = () => {
-  const navigate = useNavigate();
+  //const navigate = useNavigate();
   const [count, setCount] = useState(0);
   const [showVitoria, setShowVitoria] = useState(true);
   const [showHomem, setShowHomem] = useState(true);
@@ -18,42 +17,51 @@ export const JogoOne = () => {
   const [parada, setParada] = useState(false);
   const [timeElapsed, setTimeElapsed] = useState(0);
   const [currentStep, setCurrentStep] = useState(0);
+  const [timerRunning, setTimerRunning] = useState(false);
   const atendimentos = [
     "Vai andando até a vítima, se paramenta com luvas e máscara",
     "Personagem Vitória ficará de joelhos ao lado da vítima",
     "Personagem Vitória colocará suas duas mãos nos ombros da vítima"
   ];
-const equipamentos=['/src/img/luva.png', '/src/img/joelho.png', '/src/img/respiracao.png'];
+  const equipamentos=['/src/img/luva.png', '/src/img/joelho.png', '/src/img/respiracao.png'];
+  useEffect(() => {
+    if (timerRunning) {
+      const timer = setInterval(() => {
+        setTimeElapsed(prevTime => prevTime + 1);
+      }, 1000);
 
+      return () => clearInterval(timer);
+    }
+  }, [timerRunning]);
 
   useEffect(() => {
-    const timer = setInterval(() => {
-      setTimeElapsed(prevTime => prevTime + 1);
-    }, 1000);
-
-    return () => clearInterval(timer);
-  }, []);
-
-  useEffect(() => {
-    if (count === 10) {
+    if (count >= 100 && count <= 200) {
+      setTimerRunning(false);
+      alert('PARABÉNS! VOCÊ SALVOU UMA PESSOA!')
+    } else if (timeElapsed > 60) {
+      // Reiniciar as compressões se o tempo decorrido for superior a 1 minuto
+      setCount(0);
+      setTimerRunning(false);
+      setParada(false); // Volte para a etapa inicial se necessário
       setShowVitoria(true);
       setShowHomem(true);
       setShowCardio(false);
-      setParada(false);
+      setTimeElapsed(0)
     }
-  }, [count]);
+  }, [count, timeElapsed]);
 
   const handleButtonClick = () => {
     if (!showCardio) {
       setShowVitoria(false);
       setShowHomem(false);
       setShowCardio(true);
+      setTimerRunning(true); // Iniciar o temporizador quando as compressões começarem
     }
     setCount(prevCount => prevCount + 1);
   };
 
   const handleNextStep = () => {
-    if (currentStep < atendimentos.length - 1) {
+    if (currentStep < 2) {
       setCurrentStep(prevStep => prevStep + 1);
     } else {
       setParada(true);
@@ -68,21 +76,21 @@ const equipamentos=['/src/img/luva.png', '/src/img/joelho.png', '/src/img/respir
 
           <h2>Passo {currentStep + 1}</h2>
           <p>{atendimentos[currentStep]}</p>
-<img src={ equipamentos[currentStep]} />
+          <img src={ equipamentos[currentStep]} />
           <button onClick={handleNextStep}>Próximo Passo</button>
         </div>
       ) : (
         <div>
-          <p>Voce pegou estes equipamento, é hora de realizar ação colocando em ordem alguns passos.</p>
-          <p>Pegue </p>
+          <p>Compressão.</p>
+          <p>Tempo decorrido: {timeElapsed} segundos</p>
+
           {showVitoria && <img src={vitoria} className="cena-one" alt="Descrição da Imagem" />}
           {showHomem && <img src={homem} className="cena-one" alt="Descrição da Imagem" />}
           {showCardio && <img src={cardio} className="cena-one-cardio" alt="Descrição da Imagem" />}
 
-          <button onClick={handleButtonClick}>Apertar</button>
-          {/* <p>Clques: {count}</p>
-          <p>Tempo decorrido: {timeElapsed} segundos</p>
-          */}
+          <button onClick={handleButtonClick}>Compressão</button>
+          <p>Compressões: {count}</p>
+          
         </div>
       )}
     </>
