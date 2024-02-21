bbf9a092bc25f857180bd316a176111acdac1916
tainamiranda2 <tainaferreira0987@gmail.com>
2024-02-21 11:13:33 -0300

    trabalhando com as paginas de curriculo

--- src/pages/idealizadores/Idealizadores.jsx
+++ src/pages/idealizadores/Idealizadores.jsx
@@ -3,16 +3,54 @@ import {  Link } from 'react-router-dom';
 export const Idealizadores= () => {
     return(
         <>
-        <div>
+        <div className='idealizadores'>
 
-       
+       <div className='idealizadores-curriculo'>
         <h2>Tainá Miranda</h2>
         <span>Estudante de sistemas de informação</span>
-        <p>Desenvolvedora Fullstack Jr</p>
+        <p>Desenvolvedora</p>
+        <a href='/curriculos/fullstack.pdf' download>
+            <button>Baixar currículo</button>
+          </a>
+
         </div>
-        <div>
+
+   
+       <div className='idealizadores-curriculo'>
         <h2>Saulo Roberto</h2>
         <span>Estudante de sistemas de informação</span>
+        <p>Designer</p>
+        <button>Baixar curriculo</button>
+        </div>
+    
+        <div className='idealizadores-curriculo'>
+        <h2>Gleyson</h2>
+        <span>Estudante colaborador</span>
+        <button>Baixar curriculo</button>
+
+        </div>
+    
+        <div className='idealizadores-curriculo'>
+        <h2>Helena</h2>
+        <span>Estudante colaboradora</span>
+        <button>Baixar curriculo</button>
+
+        </div>
+    
+        <div className='idealizadores-curriculo'>
+        <h2>Ana Flavia</h2>
+        <span>Professora orientadora</span>
+        <button>Baixar curriculo</button>
+
+        </div>
+
+        <div className='idealizadores-curriculo'>
+        <h2>Edson Camilo</h2>
+        <span>Professor Orientador</span>
+        <button>Baixar curriculo</button>
+
+        </div>
+
         </div>
         <Link to="/">Voltar</Link>
         </>
